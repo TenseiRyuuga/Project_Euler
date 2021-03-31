5e250f65767a847166232588c8cd9394af91c073
TenseiRyuuga <stijntjedelion@hotmail.com>
2021-03-31 19:50:59 +0200

    added comments

--- 10001st Prime/src/main/Calculator.java
+++ 10001st Prime/src/main/Calculator.java
@@ -7,19 +7,28 @@ public class Calculator {
 	public int calculateNumberPrime(int numberPrime) {
 		int numberOfPrimes = 0;
 		int number = 1;
-		int maxLimit = 10000000;
+		// this is the limit for both the array(limited by the int max value and it
+		// indicates the maximum number that can be checked for being prime
+		int maxLimit = 2147483647;
 		boolean[] sieve = new boolean[maxLimit];
 		for (int i = 2; i < maxLimit; i++) {
+			// in case the sieve already contains the number we skip the loop and continue
+			// with the next iteration. This will filter out any non prime numbers
 			if (sieve[i] == true)
 				continue;
 
+			// raise the number of primes by 1
 			numberOfPrimes++;
 
+			// check if we reached the desired numberPrime
 			if (numberOfPrimes == numberPrime) {
+				// if we did find the desired number of prime move it into number and break the
+				// loop so we can return it
 				number = i;
 				break;
 			}
 
+			// add multiples of the number to the sieve
 			for (int j = i + i; j < maxLimit; j += i) {
 				sieve[j] = true;
 			}
